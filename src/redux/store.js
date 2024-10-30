5996e9ea7f04e8745330b34c1ea5f4ced934c388
NeiltonSeguins <seguins.neilton@gmailcom>
2024-10-30 15:12:20 -0300

    Vídeo 2.2 - Persistindo os dados do redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import usuarioSlice from "./slices/usuarioSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+import rootReducer from "./rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    usuario: usuarioSlice,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export default store;
