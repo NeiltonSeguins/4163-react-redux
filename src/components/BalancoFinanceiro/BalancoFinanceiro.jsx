c771be3b937b6ebb2b51aadba0c256c9b71c29d0
NeiltonSeguins <seguins.neilton@gmailcom>
2024-11-01 15:31:10 -0300

    Vídeo 5.2 - Juntando os dados do gráfico

--- src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
+++ src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
@@ -29,12 +29,35 @@ export const AreaChart = styled.div`
 `;
 
 const BalancoFinanceiro = () => {
+  const data = {
+    datasets: [
+      {
+        label: "Gastos por categoria",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <Cartao>
       <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
       <CartaoCorpo>
         <AreaChart>
-          <Pie />
+          <Pie data={data} />
         </AreaChart>
       </CartaoCorpo>
     </Cartao>
